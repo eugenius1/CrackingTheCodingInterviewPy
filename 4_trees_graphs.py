84899557ff07aa9eb29a3acfec4d20f34e697134
eugenius1 <esebi95@gmail.com>
2016-04-02 23:48:14 +0100

    ch4: Binary tree class

--- 4_trees_graphs.py
+++ 4_trees_graphs.py
@@ -1,3 +1,33 @@
 # Eusebius N.
 
 # 4.1
+# Check if a binary tree is balanced
+
+class TreeNode(object):
+	"""Binary Tree Node"""
+	def __init__(self, data):
+		super(TreeNode, self).__init__()
+		self.data = data
+		self.left = None
+		self.right = None
+
+	def isBalanced(self):
+		return abs(self._height(self.left) - self._height(self.right)) <= 1
+
+	def _height(self, subtree):
+		if subtree == None:
+			return 0
+		leftHeight = _height(subtree.left)
+		rightHeight = _height(subtree.right)
+		return 1 + max(leftHeight, rightHeight)
+
+	def printNode(self):
+		print self.data
+
+	def printTree(self):
+		printTree(self.left)
+		self.printNode()
+		printTree(self.right)
+
+## Testing
+print TreeNode(0).isBalanced()
